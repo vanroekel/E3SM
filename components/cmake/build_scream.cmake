1e25dd804db4a2a404374b343db1b90060fa1d0c
Xingqiu Yuan <xyuan@anl.gov>
2021-09-17 09:04:10 -0400

    add build system integration

--- components/cmake/build_scream.cmake
+++ components/cmake/build_scream.cmake
@@ -3,10 +3,13 @@
 
 function(build_scream)
 
-  # Include machine file here
-  message("Found scream component")
-  include(${CMAKE_SOURCE_DIR}/scream/cmake/machine-files/${MACH}.cmake)
-  add_subdirectory("scream")
-# set (E3SM_INTERNAL_KOKKOS_ALREADY_BUILT TRUE)
+# if (USE_SAMXX  OR COMP_NAMES MATCHES ".*scream.*") 
+    # Include machine file here
+    message("Found scream component")
+    include(${CMAKE_SOURCE_DIR}/scream/cmake/machine-files/${MACH}.cmake)
+    add_subdirectory("scream")
+    set(SCREAM_SOURCE_DIR ${CMAKE_SOURCE_DIR}/scream/src CACHE STRING "Scream Source Dir")
+    set(SCREAM_BIN_DIR ${CMAKE_CURRENT_BINARY_DIR}/scream CACHE STRING "Scream Binary Dir")
+# endif()
 
 endfunction(build_scream)
