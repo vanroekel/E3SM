d1ab93664892f6f91647898ebd55c791cd95ca15
lvanroekel <luke.vanroekel@gmail.com>
2022-11-07 22:09:26 -0700

    Introduces agi

---
+++ components/eam/src/physics/cam/agi_intr.F90
@@ -0,0 +1,112 @@
+module agi_intr
+
+  !-----------------------------------------------------------------!
+  ! Module to include silver iodide in E3SM simulations
+  !
+  !
+  !-----------------Code history------------------------------------!
+  ! Authors: Luke Van Roekel
+  !
+  !-----------------------------------------------------------------!
+
+  use shr_kind_mod, only: r8=>shr_kind_r8
+  use shr_log_mod,  only: errMsg=>shr_log_errMsg
+  use phys_control, only: phys_getopts
+  use physconst,    only: pi
+  use cam_logfile,  only: iulog
+  use spmd_utils,   only: masterproc
+  use perf_mod,     only: t_startf, t_stopf
+  use mpishorthand
+  use cam_history_support, only: fillvalue
+
+  implicit none
+
+  private
+  save
+
+  ! ----------------- !
+  ! Public interfaces !
+  ! ----------------- !
+
+  public :: agi_register,  & ! add silver iodide to pbuf
+            agi_readnl,    & ! read namelist related to silver iodide
+            agi_init_cnst, & ! initialize a constant point source of silver iodide
+            agi_emiss_data   ! read emission from data with times.
+
+  integer :: agi_idx
+
+  !namelist variables
+  logical :: agi_enable = .false. ! AgI is off by default
+  logical :: agi_point_source_emis = .true. ! if true use a fixed constant in time point source
+  logical :: agi_data_emis = .false. ! if true do emission by data file
+  logical :: agi_microphysics = .false. ! if true add impact of agi to MG2
+  real(r8) :: agi_lat = huge(1.0_r8)
+  real(r8) :: agi_lon = huge(1.0_r8)
+  real(r8) :: agi_emis_rate = huge(1.0_r8) ! emission of AgI at a point source in kg/s
+  character(len=*) :: agi_emis_data_file = 'agi_emission.bin'
+
+  contains
+
+  subroutine agi_readnl(nlfile)
+
+    use namelist_utils,  only: find_group_name
+    use units,           only: getunit, freeunit
+
+    character(len=*), intent(in) :: nlfile
+    character(len=*), parameter :: subname = 'agi_read_nl'
+
+    integer :: unitn, ierr
+
+    namelist /agi_nl/ agi_enable, agi_point_source_emis, agi_data_emis,  &
+                      agi_microphysics, agi_lat, agi_lon, agi_emis_rate, &
+                      agi_emis_data_file
+
+    if (masterproc) then
+       unitn = getunit()
+       open( unitn, file=trim(nlfile), status='old' )
+       call find_group_name(unitn, 'agi_nl', status='ierr' )
+       if (ierr == 0) then
+          read(unitn, agi_nl, iostat=ierr)
+          if (ierr /= 0) then
+             call endrun(subname // ':: ERROR reading namelist')
+          end if
+       end if
+       close(unitn)
+       call freeunit(unitn)
+
+       if (agi_enable .and. agi_point_source_emis .and.  agi_data_emis) then
+         call endrun(subname // ':: ERROR cannot enable both point source and data emissions')
+       end if
+
+       if (agi_enable .and. .not. agi_point_source_emis .and. .not. agi_data_emis) then
+         call endrun(subname // ':: ERROR must enable either point source or data emissions for AgI')
+       end if
+
+    end if
+
+#ifdef SPMD
+    call mpibcast(agi_enable,            1, mpilog, 0, mpicom)
+    call mpibcast(agi_point_source_emis, 1, mpilog, 0, mpicom)
+    call mpibcast(agi_data_emis,         1, mpilog, 0, mpicom)
+    call mpibcast(agi_microphysics,      1, mpilog, 0, mpicom)
+    call mpibcast(agi_lat,               1, mpir8,  0, mpicom)
+    call mpibcast(agi_lon,               1, mpir8,  0, mpicom)
+    call mpibcast(agi_emis_rate,         1, mpir8,  0, mpicom)
+    call mpibcast(agi_emis_data_file,    128, mpichar, 0, mpicom)
+#endif
+
+  end subroutine agi_readnl
+
+!=============================================================================================
+
+  subroutine agi_register
+
+    ! Register Agi in the physics buffer
+
+    call pbuf_add_field('AgI', 'global', dtype_r8, (/pcols,pver/), agi_idx)
+
+  end subroutine agi_register
+
+!==============================================================================================
+
+
