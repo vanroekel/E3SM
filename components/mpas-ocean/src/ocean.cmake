564f8dcd90eb17df6235418c92586a4666f10933
vanroekel <luke.vanroekel@gmail.com>
2024-06-03 21:54:30 -0700

    fixes build scripts

--- components/mpas-ocean/src/ocean.cmake
+++ components/mpas-ocean/src/ocean.cmake
@@ -34,7 +34,7 @@ list(APPEND RAW_SOURCES
   core_ocean/mode_forward/mpas_ocn_time_integration_lts.F
   core_ocean/mode_forward/mpas_ocn_time_integration_fblts.F
   core_ocean/mode_forward/mpas_ocn_time_integration_split_ab2.F
-
+  core_ocean/shared/mpas_ocn_overflow.F
   core_ocean/mode_analysis/mpas_ocn_analysis_mode.F
 
   core_ocean/shared/mpas_ocn_init_routines.F
