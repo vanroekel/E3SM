383c29fcbb7d2c0aa7eb81ae0763abb6f2ad819b
Hyun Kang <kangh@ornl.gov>
2026-05-11 00:46:30 -0400

    Implement vertical mixing tendency
    - Implemented implicit vertical mixing using TriDiagDiffSolver.
    - Added applyVertMixImplicit to VertMix.
    - Applied vertical mixing once at the end of each time step.
    - Note: Tangential velocity is temporarily handled in VertMix.

--- components/omega/test/ocn/StateTest.cpp
+++ components/omega/test/ocn/StateTest.cpp
@@ -104,6 +104,9 @@ void initStateTest() {
    VertMix::init();
 
    // Create tendencies
+   VertMix::init();
+
+   // Create tendencies
    Tendencies::init();
 
    // Finish time stepper initialization
